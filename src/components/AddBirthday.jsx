bdf8ca6ce8257b212193596c6aeba01d2065c468
Anjil Neupane <angilneupane222@gmail.com>
2023-07-03 11:28:30 +0545

    Day 8

--- src/components/AddBirthday.jsx
+++ src/components/AddBirthday.jsx
@@ -1,8 +1,38 @@
-import React from "react";
 import "./css/addbirthday.css";
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
 
 const AddBirthday = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    message: "",
+    birthDate: "2015-09-13",
+    photo: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    //Retrive existing form data from local storage
+    const existingData = JSON.parse(localStorage.getItem("formData")) || [];
+    console.log(JSON.parse(localStorage.getItem("formData")));
+    
+    //Add new form data to the array
+    const newdata = [...existingData, ...formData];
+    console.log(JSON.stringify(newdata));
+
+    //save updated data in a local Storage as JSON
+    localStorage.setItem("formData", JSON.stringify(newdata));
+    console.log(JSON.parse(localStorage.getItem("formData")));
+
+  };
   const style = {
     textAlign: "center",
     margin: "21px auto",
@@ -15,21 +45,27 @@ const AddBirthday = () => {
 
   return (
     <>
-      <form style={style}>
+      <form style={style} onSubmit={handleFormSubmit}>
         <div className="div">
           <label>First Name:</label>
           <input
+            name="firstName"
             className="input"
             type="text"
             placeholder="Firstname"
+            value={formData.firstName}
+            onChange={handleInputChange}
             required
           />
         </div>
         <div className="div">
           <label>Last Name:</label>
           <input
+            name="lastName"
             className="input"
             type="text"
+            value={formData.lastName}
+            onChange={handleInputChange}
             placeholder="Lastname"
             required
           />
@@ -37,24 +73,40 @@ const AddBirthday = () => {
         <div className="div">
           <label>Message:</label>
           <textarea
+            name="message"
+            value={formData.message}
             className="textArea"
-            name=""
-            id="1"
             cols="30"
             rows="5"
+            onChange={handleInputChange}
           ></textarea>
         </div>
         <div className="div">
           <label>Birth Date:</label>
-          <input className="input" type="date" />
+          <input
+            className="input"
+            name="birthDate"
+            value={formData.birthDate}
+            onChange={handleInputChange}
+            type="date"
+          />
         </div>
         <div className="div">
           <label>Upload Photo:</label>
-          <input className="input" type="file" aria-required />
+          <input
+            className="input"
+            name="photo"
+            value={formData.photo}
+            onChange={handleInputChange}
+            type="text"
+            aria-required
+          />
         </div>
-        <Link to="/All-birthday">
-          <button className="btn">Add</button>
-        </Link>
+        {/* <Link to="/All-birthday"> */}
+        <button className="btn">
+          <Link to="/All-birthday">Add</Link>
+        </button>
+        {/* </Link> */}
         <button className="btn" type="Clear">
           Clear
         </button>
